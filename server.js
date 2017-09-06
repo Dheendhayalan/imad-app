b5971ea995e07034635e9e47ab421ff7fc9607fc
Dheendhayalan <31158475+Dheendhayalan@users.noreply.github.com>
2017-09-06 11:04:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ function hash (input, salt){
     var hashed= crypto.pbkdf2Sync(input, salt, 10000 ,512,'sha512');
     return hashed;
 }
-app.get('/hash/input',function(req,res){
+app.get('/hash/;input',function(req,res){
     var hashedString = hash(req.params.input, 'this-is-same-return-string');
     res.send(hashedString);
 });
