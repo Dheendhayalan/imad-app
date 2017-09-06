fb20cdde73c8123649a96c193443d8eb95728158
Dheendhayalan <31158475+Dheendhayalan@users.noreply.github.com>
2017-09-06 09:49:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     database:'daya15edu',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password :process.emv.DB_PASSWORD
+    password :process.env.DB_PASSWORD
 };
 var app = express();
 app.use(morgan('combined'));
